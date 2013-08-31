83abdbe86492993cef14add6fb190b5d63e24d49
irysius <lestersy@hotmail.com>
2013-08-31 01:27:00 -0400

    added PongHub.cs, more skeleton code.

---
+++ SignalRPong/Hubs/PongHub.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+
+namespace SignalRPong.Hubs
+{
+    /// <summary>
+    /// SignalR hub class for the pong game.
+    /// </summary>
+    /// <remarks>
+    /// Valid javascript methods:
+    /// updateMessage(string message);
+    /// updatePositions(double p1, double p2, double bx, double by);
+    /// updateScore(int s1, int s2);
+    /// onRoomRegistered(string roomId, int playerId);
+    /// onGameStartable();
+    /// onGameUnstartable();
+    /// </remarks>
+    [HubName("PongHub")]
+    public class PongHub : Hub
+    {
+        public void JoinGame(string displayName)
+        {
+
+        }
+
+        public void LeaveGame()
+        {
+ 
+        }
+
+        public void StartGame()
+        {
+ 
+        }
+    }
+}
